da6427d48ac6f0f0bdc9ea908e89d2c714116a59
witchicken <witchicken2@gmail.com>
2023-02-14 16:20:17 +0900

    ing-onetimeticket

--- src/components/SeatingPlan.vue
+++ src/components/SeatingPlan.vue
@@ -15,68 +15,151 @@
         ref="seatList"
       >
         <li
-          :class="item.id"
-          v-for="item in seatMapTest"
+          v-for="item in seatMap"
           :key="item.id"
           :data-id="item.id"
+          :class="getClasses(item)"
+          :style="{
+            left: `${item.coords[0]}px`,
+            top: `${item.coords[1]}px`,
+            width: `${item.coords[2] - item.coords[0]}px`,
+            height: `${item.coords[3] - item.coords[1]}px`,
+          }"
+          @click="seatclick(item.id)"
         >
-          {{ item.coords }}
+          {{ item.id }}
         </li>
       </ul>
     </div>
+    <div
+      style="
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 400px;
+        height: 300px;
+        background-color: white;
+        border-radius: 10%;
+      "
+      v-if="isPopupVisible"
+    >
+      <div
+        style="
+          position: absolute;
+          right: 20px;
+          top: 10px;
+          font-size: 30px;
+          cursor: pointer;
+        "
+        @click="exitPopup"
+      >
+        X
+      </div>
+      <p class="text-center fs-1 mt-4">이용권 결제</p>
+      <div class="d-flex flex-column">
+        <button
+          type="button"
+          class="btn btn-danger"
+          style="width: 360px; height: 50px; transform: translateX(20px)"
+          @click="goToOneTimePay"
+        >
+          1회 이용권
+        </button>
+        <button
+          type="button"
+          class="btn btn-danger mt-4"
+          style="width: 360px; height: 50px; transform: translateX(20px)"
+          @click="goToTimeLimitPay"
+        >
+          정액제 이용권
+        </button>
+        <button
+          type="button"
+          class="btn btn-danger mt-4"
+          style="width: 360px; height: 50px; transform: translateX(20px)"
+          @click="goToDayLimitPay"
+        >
+          기간제 이용권
+        </button>
+      </div>
+    </div>
+    <div
+      style="
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 400px;
+        height: 300px;
+        background-color: white;
+        border-radius: 10%;
+      "
+      v-if="isOneTimeVisible"
+    >
+      <!--여기 작성 중-->
+    </div>
+    <div
+      style="
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 400px;
+        height: 300px;
+        background-color: white;
+        border-radius: 10%;
+      "
+      v-if="isTimeLimitVisible"
+    ></div>
+    <div
+      style="
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 400px;
+        height: 300px;
+        background-color: white;
+        border-radius: 10%;
+      "
+      v-if="isDayLimitVisible"
+    ></div>
   </div>
 </template>
 
 <script>
 export default {
   name: "SeatingPlan",
+
   data() {
     return {
-      seatMapTest: {
-        seat1: { id: 1, coords: [74, 521, 107, 554] },
-        seat2: { id: 2, coords: [74, 460, 107, 495] },
-        seat3: { id: 3, coords: [74, 403, 107, 437] },
-        seat4: { id: 4, coords: [74, 342, 107, 377] },
-        seat5: { id: 5, coords: [76, 238, 103, 275] },
-        seat6: { id: 6, coords: [74, 150, 105, 190] },
-        seat7: { id: 7, coords: [45, 94, 77, 127] },
-        seat8: { id: 8, coords: [45, 35, 78, 70] },
-        seat9: { id: 9, coords: [205, 35, 239, 70] },
-        seat10: { id: 10, coords: [204, 93, 239, 130] },
-        seat11: { id: 11, coords: [176, 151, 206, 190] },
-        seat12: { id: 12, coords: [506, 239, 537, 278] },
-        seat13: { id: 13, coords: [558, 239, 588, 278] },
-        seat14: { id: 14, coords: [676, 270, 709, 307] },
-        seat15: { id: 15, coords: [676, 331, 709, 365] },
-        seat16: { id: 16, coords: [676, 387, 709, 425] },
-        seat17: { id: 17, coords: [676, 445, 709, 481] },
-        seat18: { id: 18, coords: [558, 426, 588, 464] },
-        seat19: { id: 19, coords: [505, 451, 539, 486] },
-      },
+      isPopupVisible: true,
+      isOneTimeVisible: false,
+      isTimeLimitVisible: false,
+      isDayLimitVisible: false,
+
       seatMap: {
-        seat1: [74, 521, 107, 554],
-        seat2: [74, 460, 107, 495],
-        seat3: [74, 403, 107, 437],
-        seat4: [74, 342, 107, 377],
-        seat5: [76, 238, 103, 275],
-        seat6: [74, 150, 105, 190],
-        seat7: [45, 94, 77, 127],
-        seat8: [45, 35, 78, 70],
-        seat9: [205, 35, 239, 70],
-        seat10: [204, 93, 239, 130],
-        seat11: [176, 151, 206, 190],
-        seat12: [506, 239, 537, 278],
-        seat13: [558, 239, 588, 278],
-        seat14: [676, 270, 709, 307],
-        seat15: [676, 331, 709, 365],
-        seat16: [676, 387, 709, 425],
-        seat17: [676, 445, 709, 481],
-        seat18: [558, 426, 588, 464],
-        seat19: [505, 451, 539, 486],
+        seat1: { id: 1, coords: [74, 521, 107, 554], isUsing: false },
+        seat2: { id: 2, coords: [74, 460, 107, 495], isUsing: false },
+        seat3: { id: 3, coords: [74, 403, 107, 437], isUsing: false },
+        seat4: { id: 4, coords: [74, 342, 107, 377], isUsing: false },
+        seat5: { id: 5, coords: [76, 238, 103, 275], isUsing: false },
+        seat6: { id: 6, coords: [74, 150, 105, 190], isUsing: false },
+        seat7: { id: 7, coords: [45, 94, 77, 127], isUsing: false },
+        seat8: { id: 8, coords: [45, 35, 78, 70], isUsing: false },
+        seat9: { id: 9, coords: [205, 35, 239, 70], isUsing: false },
+        seat10: { id: 10, coords: [204, 93, 239, 130], isUsing: false },
+        seat11: { id: 11, coords: [176, 151, 206, 190], isUsing: false },
+        seat12: { id: 12, coords: [506, 239, 537, 278], isUsing: false },
+        seat13: { id: 13, coords: [558, 239, 588, 278], isUsing: false },
+        seat14: { id: 14, coords: [676, 270, 709, 307], isUsing: false },
+        seat15: { id: 15, coords: [676, 331, 709, 365], isUsing: false },
+        seat16: { id: 16, coords: [676, 387, 709, 425], isUsing: false },
+        seat17: { id: 17, coords: [676, 445, 709, 481], isUsing: false },
+        seat18: { id: 18, coords: [558, 426, 588, 464], isUsing: false },
+        seat19: { id: 19, coords: [505, 451, 539, 486], isUsing: false },
       },
-      mapSeat: [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-      ],
       nowUsingSeat: [],
     };
   },
@@ -94,52 +177,94 @@ export default {
       return this.$store.state.isLoggedIn;
     },
   },
-  create() {
+  created() {
     //사용중인 좌석 가져오기
+    //기간지났을시 시간,좌석 초기화작업
+
     //가져와서 nowUsingSeat에 넣기
+    this.getSeat();
   },
-  mounted() {
-    console.dir(this.$refs.seatList);
-    /*this.mapSeat.forEach((seatNumber) => {
-      this.makeChildNode(seatNumber);
-    });*/
-  },
+  mounted() {},
   methods: {
+    exitPopup() {
+      this.isPopupVisible = false;
+    },
     useSeat(seatnumber) {
       console.log(seatnumber);
     },
-    makeChildNode(seatNumber) {
-      const seatList = this.$refs.seatList;
-      const childNode = document.createElement("li");
+    getClasses(item) {
+      return {
+        isusing: item.isUsing,
+        seat: true,
+      };
+    },
+    goToOneTimePay() {
+      this.isPopupVisible = false;
+      this.isOneTimeVisible = true;
+    },
+    goToTimeLimitPay() {
+      this.isPopupVisible = false;
+      this.isTimeLimitVisible = true;
+    },
+    goToDayLimitPay() {
+      this.isPopupVisible = false;
+      this.isDayLimitVisible = true;
+    },
 
-      childNode.setAttribute(
-        "style",
-        `position: absolute;
-            left: ${this.seatMap[`seat${seatNumber}`][0]}px;
-            top: ${this.seatMap[`seat${seatNumber}`][1]}px;
-            right: ${this.seatMap[`seat${seatNumber}`][2]}px;
-            bottom: ${this.seatMap[`seat${seatNumber}`][3]}px;
-            width: ${
-              this.seatMap[`seat${seatNumber}`][2] -
-              this.seatMap[`seat${seatNumber}`][0]
-            }px;
-            height: ${
-              this.seatMap[`seat${seatNumber}`][3] -
-              this.seatMap[`seat${seatNumber}`][1]
-            }px;
-            background-color: gray;
-            color:white;
-            font-size: 14px;
-            text-align:center;
-            cursor:pointer; 
-            `
-      );
-
-      childNode.setAttribute("class", `seat${seatNumber}`);
-      //좌석 사용중일경우 usingseat 클래스 추가
-      childNode.innerText = `${seatNumber}`;
-
-      seatList.appendChild(childNode);
+    resetExpireSeat() {},
+    async getSeat() {
+      try {
+        await this.$api("/api/getseat", {}).then((res) => {
+          const filteredRes = res.getseat.filter((item) => {
+            return item.isusing > 0;
+          });
+          const nowDate = new Date();
+          filteredRes.forEach(async (elem, index) => {
+            //resetseat
+            if (elem.enddate !== null && elem.enddate < nowDate) {
+              await this.$api("/api/resetseat", { param: elem.seatnumber });
+              await filteredRes.splice(index, 1);
+            } else {
+              this.seatMap[`seat${elem.seatnumber}`].isUsing = true;
+            }
+          });
+        });
+      } catch (error) {
+        console.err(error);
+      }
+    },
+    seatclick(seatnumber) {
+      console.log(seatnumber);
+      //fire하고 db로 보내기
+      console.log(this.seatMap[`seat${seatnumber}`].isUsing);
+      if (this.seatMap[`seat${seatnumber}`].isUsing) {
+        this.$swal.fire("이미 사용중인 좌석입니다.");
+      } else {
+        this.$swal
+          .fire({
+            title: `${seatnumber}번 좌석`,
+            confirmButtonText: "사용하기",
+            cancelButtonText: "취소",
+          })
+          .then(async (result) => {
+            if (result.isConfirmed) {
+              await this.$api("/api/getusertickettype", {
+                param: [this.storeUser.uid],
+              }).then((res) => {
+                if (res.getusertickettype[0].tickettype == 0) {
+                  //티켓이 없을경우
+                  this.isPopupVisible = true;
+                } else if (res.getusertickettype[0].tickettype == 1) {
+                  //1회용
+                } else if (res.getusertickettype[0].tickettype == 2) {
+                  //정액
+                } else if (res.getusertickettype[0].tickettype == 3) {
+                  //기간
+                }
+              });
+            }
+          });
+      }
     },
   },
 };
@@ -150,7 +275,15 @@ ul,
 li {
   list-style: none;
 }
-.usingseat {
+.seat {
+  position: absolute;
+  background-color: gray;
+  color: white;
+  font-size: 14px;
+  text-align: center;
+  cursor: pointer;
+}
+.isusing {
   background-color: brown !important;
 }
 </style>
